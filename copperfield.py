4dccc5a1c144fe9604d98578db1516ef0e501179
claudionanni <claudio.nanni@gmail.com>
2017-08-10 19:02:32 +0200

    added first 2 chars from original to respect ordering

--- copperfield.py
+++ copperfield.py
@@ -7,7 +7,7 @@ import random
 
 for line in fileinput.input():
     # Replace Names
-    line = re.sub(r'`.((?!`).)*`', lambda x: ''.join([' `',hashlib.md5(x.group().encode('utf8')).hexdigest(),'` ']),line.rstrip())
+    line = re.sub(r'`(.((?!`).)*)`', lambda x: ''.join([' `',x.group(1)[0:2],hashlib.md5(x.group(1).encode('utf8')).hexdigest()[0:20],'` ']),line.rstrip())
    
     # VARCHAR fields that are too short contain md5 hashes that can collide and this is a problem especially if the field is a key or part of, we replace this with VARCHAR(10), increase if duplicate keys occurr
     line2 = re.sub(r'VARCHAR\((\d)\)',r'VARCHAR(10)' ,line.rstrip(),flags=re.IGNORECASE)
@@ -15,5 +15,5 @@ for line in fileinput.input():
     #line3 = re.sub(r'\'\),',r'\'\),  ', line2.rstrip())
 
     # Replace Data
-    line4 = re.sub(r',\'.((?!((,\')|(\),\())).)*\'', lambda x: ''.join([",'",hashlib.md5(x.group().encode('utf8')).hexdigest()[0:40],"'"]),line2.rstrip())
+    line4 = re.sub(r',\'(.((?!((,\')|(\),\())).)*)\'', lambda x: ''.join([",'",x.group(1)[0:3],hashlib.md5(x.group(1).encode('utf8')).hexdigest()[0:20],"'"]),line2.rstrip())
     print(line4)
