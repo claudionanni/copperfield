063bc73b7f6831cecc3e6c509a05fbb7e9ed2f40
claudionanni <claudio.nanni@gmail.com>
2017-08-14 09:11:25 +0200

    refresh

--- copperfield.py
+++ copperfield.py
@@ -14,8 +14,8 @@ for line in fileinput.input():
     # Remove Host and Database
     line = re.sub(r'^-- Host. \d+.\d+.\d+.\d+ +Database: .*','-- Host: xxx.xxx.xxx.xxx     Database: XXXXXXXX',line.rstrip())
 
-    # Replace Names, I keep the first two characters of the identificators for easier reference to original ones
-    line = re.sub(r'`(.((?!`).)*)`', lambda x: ''.join(['`',x.group(1)[0:2],hashlib.md5(x.group(1).encode('utf8')).hexdigest()[0:20],'`']),line.rstrip())
+    # Replace Names, I keep the first two characters of the identifiers for easier reference to original ones. As identifiers are less numerous than data I can pretend 10 characters only of the md5 hash are enough to avoid collisions.
+    line = re.sub(r'`(.((?!`).)*)`', lambda x: ''.join(['`',x.group(1)[0:2],hashlib.md5(x.group(1).encode('utf8')).hexdigest()[0:10],'`']),line.rstrip())
    
     # VARCHAR fields that are too short contain md5 hashes that can collide and this is a problem especially if the field is a key or part of, we replace this with VARCHAR(10), increase if duplicate keys occurr
     line2 = re.sub(r'VARCHAR\((\d)\)',r'VARCHAR(10)' ,line.rstrip(),flags=re.IGNORECASE)
@@ -23,6 +23,7 @@ for line in fileinput.input():
     #line3 = re.sub(r'\'\),',r'\'\),  ', line2.rstrip())
 
     # Replace Data, I do not touch numbers, and I keep the first two characters of the original string to maintain a minimal ordering.
-    # For now we keep the strings at fixed size, 3 + 20, for more realistic testing scenarios it's possible we'll need to to make the len(hash)=len(string)
-    line4 = re.sub(r',\'(.((?!((,\')|(\),\())).)*)\'', lambda x: ''.join([",'",x.group(1)[0:3],hashlib.md5(x.group(1).encode('utf8')).hexdigest()[0:20],"'"]),line2.rstrip())
+    # For now we keep the strings at fixed size, 3 + 21, for more realistic testing scenarios it's possible we'll need to to make the len(hash)=len(string)
+    # I assume that 21 characters of the md5 hash are enough to avoid collisions, if it's not the case with a specific dataset, increase it.
+    line4 = re.sub(r',\'(.((?!((,\')|(\),\())).)*)\'', lambda x: ''.join([",'",x.group(1)[0:3],hashlib.md5(x.group(1).encode('utf8')).hexdigest()[0:21],"'"]),line2.rstrip())
     print(line4)
